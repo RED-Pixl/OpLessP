7972e1234c600e402c324b160eb4584c7f30124a
RED-Pixl <108759893+RED-Pixl@users.noreply.github.com>
2023-04-22 15:28:06 +0200

    Setup a simple API for testing purposes and removed database.py.

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,20 @@
-import os
-
-from flask import Flask, escape, session, request, redirect
-
-from backend import database
+from flask import Flask
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
 
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if request.method == "POST":
-        session["name"] = request.form["name"]
-        return redirect(request.url)
-    else:
-        if "name" in session:
-            return "Hallo " + escape(session["name"])
-        else:
-            return """
-                <form method="post">
-                    <p><input type=text, name=name>
-                    <p><input type=submit value=Login>
-                </from>
-            """
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"}
+
+    def post(self):
+        return {"data": "Posted"}
 
 
-app.secret_key = os.urandom(32)  # change os.urandom(32) to a secret key
+api.add_resource(HelloWorld, "/helloworld")
+
 
 if __name__ == "__main__":
-    database.create()
-    app.run(port=1337, debug=True)
+    app.run(debug=True)
