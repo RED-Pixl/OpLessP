490794dd2ed33c9b89b9281c66e6588dbe8a33ca
RED-Pixl <108759893+RED-Pixl@users.noreply.github.com>
2023-04-24 15:16:37 +0200

    Simplified the test code for the api

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,11 @@
 from flask import Flask
-from flask_restful import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"data": "Hello World"}
-
-    def post(self):
-        return {"data": "Posted"}
-
-
-api.add_resource(HelloWorld, "/helloworld")
+@app.route("/test", methods=["GET"])
+def test():
+    return {"data": "Hello World"}
 
 
 if __name__ == "__main__":
